65b6524e7a4c33a17c5ff09d3898b8dd26ebcbf0
nusratjahanlima <nusratjahanlima639@gmail.com>
2024-02-16 20:33:12 +0600

    updated life and score section

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -10,28 +10,49 @@
     //console.log(playgroundSection.classList)
 } */
 
-function handleKeyboardButtonPress(event){
-    const playerPressed = event.key;
-    console.log('player pressed', playerPressed);
 
-    //get the expected to press
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+    console.log( 'player pressed', playerPressed)
+
+    // key player is expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
-    console.log(playerPressed, expectedAlphabet);
 
-    //check matched or not
-    if(playerPressed === expectedAlphabet){
-        console.log('you get a point');
-        console.log('you have pressed corectly', expectedAlphabet);
-        removebackgroundColorById(expectedAlphabet);
-        continueGame();
+    // check right or wrong key pressed
+    if (playerPressed === expectedAlphabet) {
+        console.log('you got a point!');
+        //console.log('you have pressed correctly', expectedAlphabet);
+        //update score:
+        //1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        //2. increase the score by 1
+        const newScore = currentScore + 1;
+        //3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+         // start a new round
+         removeBackgroundColorById(expectedAlphabet);
+         continueGame();
     }
     else{
-        console.log('you missed. you lost a life');
+        console.log('You missed. You lost a life.');
+        //step 1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        //step2: reduce the life counr
+        const newLife = currentLife - 1;
+        //step3: display the updated life count
+        currentLifeElement.innerText = newLife;
     }
 }
-
 //capture keyboard key press
 document.addEventListener('keyup',handleKeyboardButtonPress);
 
@@ -52,5 +73,5 @@ function continueGame(){
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
-    continueGame()
+    continueGame();
 }
