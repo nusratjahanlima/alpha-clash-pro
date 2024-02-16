01a4711b72ffe0605bcaa770a5cc804eafbd393f
nusratjahanlima <nusratjahanlima639@gmail.com>
2024-02-16 17:56:48 +0600

    generate random number and background color

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -10,7 +10,47 @@
     //console.log(playgroundSection.classList)
 } */
 
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    //check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        console.log('you have pressed corectly', expectedAlphabet);
+        removebackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed. you lost a life');
+    }
+}
+
+//capture keyboard key press
+document.addEventListener('keyup',handleKeyboardButtonPress);
+
+function continueGame(){
+    //step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    //set randomly generated alphabet to the screen 
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+
+    //set background color
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,30 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+function getARandomAlphabet(){
+    //get or creat an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    console.log(alphabets);
+
+    //get a random index between 0-25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    //console.log(index);
+
+    const alphabet = alphabets[index];
+   // console.log(index, alphabet);
+    return alphabet;
+}
